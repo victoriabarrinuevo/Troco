705c9907c095f20f7aac98035e122bdec89dd188
Victória <victoriabarrinuevo02@gmail.com>
2026-09-25 20:47:46 -0300

    programa de cálculo finalizado

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("--Calculando troco de compra--");
+
+Console.WriteLine("Digite o valor de sua compra: ");
+double compra = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("digite o valor pago: ");
+double valorpg = Convert.ToDouble(Console.ReadLine());
+
+double troco = (valorpg - compra);
+
+Console.WriteLine($"Seu troco é igual à:{troco:N2}");
